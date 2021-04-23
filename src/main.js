6bf2eda5843cccf5b659f416de04d3c913de5996
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-23 11:04:21 -0700

    WIP parsing apikey

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Convert from './currency-exchanger';
 
-$; 
+$("button#but").on("click", (event) => {
+  event.preventDefault();
+  let promise = Convert.convertMoney();
+  promise.then(function (response) {
+    const body = JSON.parse(response);
+    ("#showResult").parseInt(body.converstion_rates.usd);
+  });
+});
