ccb9eaebaf0caddc760be14c56babd6bcb8a7ffb
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-23 09:11:59 -0700

    add initial files and environment

---
+++ src/currency-exchanger.js
@@ -0,0 +1,17 @@
+export default class Convert {
+  static convertMoney() {
+    return new Promise (function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/647b44663ab379005ab33e25/latest/USD`;
+      request.onload = function () {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
